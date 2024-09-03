e9739c86ffde278f8604c79f32e37420e987eea7
tonnymwanza <tonnimwanza@gmail.com>
2024-09-03 15:59:43 +0300

    creating the home page interface

--- atm_app/urls.py
+++ atm_app/urls.py
@@ -1,8 +1,15 @@
 from . views import HomeView
 from django.urls import path
+
+from . views import WithdrawView
+from . views import BalanceView
+from . views import DepositView
 # my urls
 
 urlpatterns = [
     path('', HomeView.as_view(),name='home'),
+    path('withdraw', WithdrawView.as_view(), name='withdraw'),
+    path('balance', BalanceView.as_view(), name='balance'),
+    path('deposit', DepositView.as_view(), name='deposit')
 ]
 
--- atm_app/views.py
+++ atm_app/views.py
@@ -2,7 +2,26 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 
+# home page view
 class HomeView(View):
 
     def get(self, request):
         return render(request, 'home.html')
+    
+# the view to render the withdraw page
+class WithdrawView(View):
+
+    def get(self, request):
+        return render(request, 'withdraw.html')
+
+# the view to render the deposit page
+class DepositView(View):
+
+    def get(self, request):
+        return render(request, 'deposit.html')
+    
+# the view to render the balance page
+class BalanceView(View):
+
+    def get(self, request):
+        return render(request, 'balance.html')

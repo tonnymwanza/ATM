541ae763f5f67341d92e3173f0536065f9ffd7ae
tonnymwanza <tonnimwanza@gmail.com>
2024-11-15 11:19:42 +0300

    creating signals for transaction model

--- atm_app/models.py
+++ atm_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 from . validators import withdrawal_validator
@@ -11,3 +13,9 @@ class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     balance_after_withdrawal = models.DecimalField(max_digits=6, decimal_places=2, null=True)
 
+
+# signal to create transaction object whenever a user is created
+@receiver(post_save, sender=User)
+def transaction_signal_fun(sender, instance, created, **kwargs):
+    if created:
+        Transaction.objects.create(user=instance)
